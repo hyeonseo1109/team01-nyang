49e574f847ed754a3e5618e3c775ba495653dc0a
hyeonseo1109 <alsgustj2@naver.com>
2025-10-07 06:17:45 +0900

    ✨ Admin 계정 삭제/차단 연결
    admin 계정 삭제/차단 연결

--- src/api/admin.js
+++ src/api/admin.js
@@ -59,8 +59,11 @@ export function useUserSearch(search) {
 //const {userSearchData, userSearchIsLoading, userSearchIsError } = userUserSearch(userId);
 
 // !- - - - 유저 계정 차단 (관리자) - - - -
-export async function updateUser({ user_Id, is_active }) {
-  const res = await api.patch(`/admin/users/${user_Id}/status`, { params: { is_active } });
+export async function updateUser({ user_id, is_active }) {
+  console.log('user_id:', user_id);
+  console.log('is_active:', is_active);
+  const res = await api.patch(`/admin/users/${user_id}/status`, {}, { params: { is_active } });
+  console.log('Request URL:', res.config.url);
   return res.data;
 }
 export function useUpdateUser() {
