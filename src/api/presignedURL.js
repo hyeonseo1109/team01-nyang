03ffe3562a2f1b9399d713caeba4100f25a865a7
hyeonseo1109 <alsgustj2@naver.com>
2025-10-10 00:22:45 +0900

    ✨ 프로필사진 - 설명 작성
    프로필사진 - 설명 작성

--- src/api/presignedURL.js
+++ src/api/presignedURL.js
@@ -1,4 +1,11 @@
 import { api } from './client';
+// Postman 사용법
+// POST https://www.nyangbiseo.store/s3/presigned-url, body(raw)에 filename(ex: "babyShark.png"), content_type(ex: "image/png")를 담아 보냄.
+// => 응답값으로 오는 data 중 upload_url 복사
+// => 저 url에 PUT으로, body에 binary로 파일을 담아서 보냄
+// => 200(성공) 뜨면
+// => 위 응답값의 file_url을 https://www.nyangbiseo.store/users/me(유저 정보 수정)으로 보냄. (ex: "profile_image": "https://~~")
+// => https://www.nyangbiseo.store/users/me GET 해보면 바뀐 프로필 사진 확인 가능함.
 
 // 1. Presigned URL 발급 (client.js에서 만든 axios 인스턴스 활용)
 export async function getPresignedUrl(filename, content_type) {
