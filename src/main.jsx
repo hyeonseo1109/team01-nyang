6a29357eb1df1d20c5c37e94d66a62db3995e407
seun9yoon <seun9yoon@gmail.com>
2025-10-11 23:17:18 +0900

    ✨ 차트 오류 수정
    차트 오류 수정

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './queryClient.js';
+import 'chart.js/auto';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <QueryClientProvider client={queryClient}>
